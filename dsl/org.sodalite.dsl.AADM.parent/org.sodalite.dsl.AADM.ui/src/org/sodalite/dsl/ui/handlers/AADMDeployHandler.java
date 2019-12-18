8daa833e4e4d1f71f98bbee001dcdd3c067dab97
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2019-12-18 16:31:14 +0100

    IDE prepared to send AADM models to the KB

---
+++ dsl/org.sodalite.dsl.AADM.parent/org.sodalite.dsl.AADM.ui/src/org/sodalite/dsl/ui/handlers/AADMDeployHandler.java
@@ -0,0 +1,53 @@
+package org.sodalite.dsl.ui.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+
+public class AADMDeployHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Get serialize AADM model in Turtle
+		// TODO Send turtle to KB
+		// TODO Request IoC API to deploy model
+		// Upon completion, show dialog
+		MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+				"Deploy AADM", "The selected AADM model has been succesfully deployed");
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ dsl/org.sodalite.dsl.AADM.parent/org.sodalite.dsl.AADM.ui/src/org/sodalite/dsl/ui/handlers/AADMSaveHandler.java
@@ -0,0 +1,55 @@
+package org.sodalite.dsl.ui.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.ui.PlatformUI;
+
+public class AADMSaveHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Get serialize AADM model in Turtle
+		// TODO Send turtle to KB
+		// Upon completion, show dialog
+		MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+				"Save AADM", "The selected AADM model has been succesfully store in the KB");
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Validate AADM before enabling
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
