9e197341e027939a2758409c8c717dc279ddefb6
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-07-01 09:49:28 +0200

    Reporting reading default backend configurations

--- dsl/org.sodalite.IDE.parent/org.sodalite.dsl.preferences/src/org/sodalite/dsl/ui/preferences/PreferenceInitializer.java
+++ dsl/org.sodalite.IDE.parent/org.sodalite.dsl.preferences/src/org/sodalite/dsl/ui/preferences/PreferenceInitializer.java
@@ -3,24 +3,34 @@ package org.sodalite.dsl.ui.preferences;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.MessageFormat;
 import java.util.Properties;
 
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * Class used to initialize default preference values.
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
+	private static final Bundle BUNDLE = FrameworkUtil.getBundle(PreferenceInitializer.class);
+	private static final ILog LOGGER = Platform.getLog(BUNDLE);
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#
+	 * initializeDefaultPreferences()
 	 */
 	public void initializeDefaultPreferences() {
-		//Read default values from a properties file located in default Eclipse root installation
+		// Read default values from a properties file located in default Eclipse root
+		// installation
 		Properties prop = readDefaultProperties();
 		String kb_reasoner_uri = prop.getProperty(PreferenceConstants.KB_REASONER_URI);
 		if (kb_reasoner_uri == null)
@@ -31,24 +41,29 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		String xOpera_uri = prop.getProperty(PreferenceConstants.xOPERA_URI);
 		if (xOpera_uri == null)
 			xOpera_uri = "http://154.48.185.206:5000/";
-		
-		ScopedPreferenceStore store = 
-	        	new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.sodalite.dsl.preferences");
-		
+
+		LOGGER.log(new Status(Status.INFO, BUNDLE.getSymbolicName(), MessageFormat.format(
+				"Default Sodalite backend services configuration read from properties: KB_Reasoner endpoint: {0}, IaC endpoint: {1}, xOpera endpoint: {2}",
+				kb_reasoner_uri, iac_uri, xOpera_uri)));
+
+		ScopedPreferenceStore store = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.sodalite.dsl.preferences");
+
 		store.setDefault(PreferenceConstants.KB_REASONER_URI, kb_reasoner_uri);
 		store.setDefault(PreferenceConstants.IaC_URI, iac_uri);
 		store.setDefault(PreferenceConstants.xOPERA_URI, xOpera_uri);
 	}
-	
+
 	private Properties readDefaultProperties() {
 		String eclipseExecutablePath = System.getProperty("eclipse.launcher");
-		String propertiesPath = eclipseExecutablePath.substring(0, eclipseExecutablePath.lastIndexOf('/') + 1) + "sodalite.properties";
+		String propertiesPath = eclipseExecutablePath.substring(0, eclipseExecutablePath.lastIndexOf('/') + 1)
+				+ "sodalite.properties";
 		Properties prop = new Properties();
 		try (InputStream input = new FileInputStream(propertiesPath)) {
-            prop.load(input);
-        } catch (IOException ex) {
-            System.out.println("sodalite.properties could not be found in Eclipse installation root folder");
-        }
+			prop.load(input);
+		} catch (IOException ex) {
+			LOGGER.log(new Status(Status.ERROR, BUNDLE.getSymbolicName(),
+					"Could not read configuration for Sodalite backend services", ex));
+		}
 		return prop;
 	}
 
