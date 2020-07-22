a149f8e4b72d1ab8acc2a97e4c599cf21a1eebea
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-07-22 10:09:05 +0200

    issue #20: added Sodalite examples project to container workspace

--- Docker/create_sodalite_ide_image.sh
+++ Docker/create_sodalite_ide_image.sh
@@ -1,2 +1,6 @@
 docker rm sodalite-ide
+#Add Sodalite examples to workspace
+mkdir workspace
+cp -r ../dsl/org.sodalite.dsl.examples/ ./workspace
 docker build --no-cache -t sodalite-ide .
+rm -rf ./workspace
