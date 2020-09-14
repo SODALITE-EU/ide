b9673fe752324a84d9900acebe6971fca9837cfc
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-09-14 15:49:16 +0200

    fix repository target folder

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cp -r dsl/org.sodalite.IDE.parent/org.sodalite.IDE.repository/target/repository/ site/repository/
+cp -r dsl/org.sodalite.IDE.parent/org.sodalite.IDE.repository/target/repository/ site/
 scripts/deploy.sh -c ./scripts/setup.env
