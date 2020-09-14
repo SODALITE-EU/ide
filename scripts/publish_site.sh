c0ad4a08dea8d4672b1c1b5316f8c47bb549bb9e
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-09-14 16:16:57 +0200

    removing site definition from site

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-cp -r dsl/org.sodalite.IDE.parent/org.sodalite.IDE.repository/target/repository/ site/
-cp scripts/_config.yml site/
-cp scripts/index.md site/
+cp -r dsl/org.sodalite.IDE.parent/org.sodalite.IDE.repository/target/repository/* site/
 scripts/deploy.sh -c ./scripts/setup.env
