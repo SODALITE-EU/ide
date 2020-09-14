0944c7ccd11c3a3eef08e784eb18205828cae2ac
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-09-14 15:53:43 +0200

    fixing copy of site files

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cp -r dsl/org.sodalite.IDE.parent/org.sodalite.IDE.repository/target/repository/ site/
+cp scripts/_config.yml site/
+cp scripts/index.md site/
 scripts/deploy.sh -c ./scripts/setup.env
