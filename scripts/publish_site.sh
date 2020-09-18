3e67017bcf8f13613123dcb7d86a07cc7719ee90
Yosu Gorroñogoitia <jesus.gorronogoitia@atos.net>
2020-09-18 13:56:48 +0200

    Preparing publish script to get GitHub account: <user> <password>

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 rm -r site/*
 cp -r dsl/org.sodalite.IDE.parent/org.sodalite.IDE.repository/target/repository/* site/
+cp scripts/setup.env.template scripts/setup.env
+echo export GIT_DEPLOY_REPO="https://$1:$2@github.com/SODALITE-EU/ide.git" >> scripts/setup.env
 scripts/deploy.sh -c scripts/setup.env -m "IDE update site"
