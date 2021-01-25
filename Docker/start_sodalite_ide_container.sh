ce71598fbf0bc862a94ea339dcc96d102a7a9eb1
kmlTE <tokmakovkamil@gmail.com>
2021-01-25 13:45:36 +0100

    add initial remodel of clinical-uc

--- Docker/start_sodalite_ide_container.sh
+++ Docker/start_sodalite_ide_container.sh
@@ -4,6 +4,6 @@ if docker ps -a -f "name=sodalite" | grep sodalite; then
 	docker start sodalite-ide
 else
 	echo 'Starting a new Sodalite container'
-	docker run --name sodalite-ide -it -d -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/sodalite.properties:/sodalite/eclipse/sodalite.properties sodalite-ide:latest 
+	docker run --name sodalite-ide -it -d -e DISPLAY=:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/sodalite.properties:/sodalite/eclipse/sodalite.properties $1 sodalite-ide:latest 
 fi
 
